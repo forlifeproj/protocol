15a18e6116a2e12012c4ae2eced260b44817f6ff
forlifeproj <forlifeproject@163.com>
2024-04-29 12:05:28 +0800

    add json demo

---
+++ gfriends/json/demo/demo.go
@@ -0,0 +1,10 @@
+package demo
+
+type Args struct {
+	A int `json:"pa"`
+	B int `json:"pb"`
+}
+
+type Reply struct {
+	C int `json:"rc"`
+}
---
+++ gfriends/json/go.mod
@@ -0,0 +1,3 @@
+module github.com/forlifeproj/protocol/gfriends/json
+
+go 1.20
