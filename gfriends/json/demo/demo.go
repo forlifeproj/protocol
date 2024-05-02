5cca986baa38e3a9e7caa3f212a4ae17cc2f8325
forlifeproj <forlifeproject@163.com>
2024-05-02 11:42:46 +0800

    增加订单模块协议

--- gfriends/json/demo/demo.go
+++ gfriends/json/demo/demo.go
@@ -8,3 +8,42 @@ type Args struct {
 type Reply struct {
 	C int `json:"rc"`
 }
+
+// 生成订单id
+type GenerateOrderIdReq struct {
+	Uid int64 `json:"uid"`
+}
+
+type GenerateOrderIdRsp struct {
+	OrderId string `json:"orderId"`
+}
+
+// 订单状态轮询接口
+type QueryOrderStatusReq struct {
+	OrderId string `json:"orderId"`
+}
+
+type QueryOrderStatusRsp struct {
+	Status       int    `json:"status"`
+	MaxPollTimes int    `json:"maxPollTimes"`
+	PollInterval int    `json:"pollInterval"`
+	NeedPoll     int    `json:"needPoll"`
+	BizInfo      string `json:"bizInfo"`
+}
+
+type OrderInfo struct {
+	OrderId string `json:"orderId"`
+}
+
+// 拉取订单列表
+type GetOrderListReq struct {
+	Uid         int64  `json:"uid"`
+	PageSize    int    `json:"pageSize"`
+	StrPassBack string `json:"strPassBack"`
+}
+
+type GetOrderListRsp struct {
+	HasMore     int         `json:"hasMore"`
+	StrPassBack string      `json:"strPassBack"`
+	OrderList   []OrderInfo `json:"orderList"`
+}
